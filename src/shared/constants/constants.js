e4b9280cb6055380c03c427e418ca9a2c8001329
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:33:20 +0300

    Added 'activeLink' CSS class for styling active navigation links

---
+++ src/shared/constants/constants.js
@@ -0,0 +1,2 @@
+export const activeLink =
+  'cursor-pointer py-3 text-sm font-montserrat  font-semibold text-main-text focus:text-accent-color border-b-[3px] border-b-accent-color transition duration-200 ease-in-out';
---
+++ src/shared/constants/index.js
@@ -0,0 +1,3 @@
+import { activeLink } from './constants';
+
+export { activeLink };
