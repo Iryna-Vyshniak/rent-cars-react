ffcdb23a13aa43f263e6ad709448a9114f9970d9
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:43:46 +0300

    created reusable image component,  designed for displaying thumbnail images with optional custom styling

---
+++ src/components/ThumbImage.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const ThumbImage = ({ src, alt, className }) => {
+  return (
+    <div className="thumb mb-[14px] max-w-full overflow-hidden rounded-[14px]">
+      <img
+        src={src}
+        alt={alt}
+        className={
+          className
+            ? `block object-cover object-center ${className}`
+            : 'block h-auto max-w-full  object-cover object-center'
+        }
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+ThumbImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+export default ThumbImage;
