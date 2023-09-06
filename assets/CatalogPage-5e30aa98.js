3df77d845b0b6eb45d8fc2a5538d70d08f9046f0
Iryna-Vyshniak <Iryna-Vyshniak@users.noreply.github.com>
2023-09-06 11:01:10 +0000

    deploy: 5b241c9543c308b781027da2fadc5f77f770042e

---
+++ assets/CatalogPage-5e30aa98.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-f14a3c74.js";const e=()=>a.jsx("div",{children:"CatalogPage"});export{e as default};
---
+++ assets/FavoritesPage-62446e2c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-f14a3c74.js";const t=()=>e.jsx("div",{children:"FavoritesPage"});export{t as default};
