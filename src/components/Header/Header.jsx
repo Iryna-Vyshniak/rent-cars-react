b94b2a5485598c6399d5391b16befa2ffeaeb35a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:47:27 +0300

    added Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <header className="header bg-white/30 backdrop-blur-mds">
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
