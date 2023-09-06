c7af1697e9f8585d16a30750adc91d38695cddff
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:39:47 +0300

    Created dynamic  mobile navigation menu component

---
+++ src/components/Header/MobileNavbar.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+import Logo from '../Logo';
+
+import Button from '../Button';
+
+import NavMenu from './NavMenu';
+
+const MobileNavbar = ({ navbar, openNav }) => {
+  return (
+    <div className={`mobile-navbar ${navbar ? 'open-nav' : ''}`}>
+      <Logo className="absolute inset-x-0 bottom-3 z-[60]" />
+      <Button
+        onClick={openNav}
+        svgClass="mobile-navbar-close"
+        iconURL="#icon-close"
+        ariaLabel="close"
+      />
+
+      <NavMenu closeNav={openNav} className="mobile-navbar-items" />
+    </div>
+  );
+};
+
+MobileNavbar.propTypes = {
+  navbar: PropTypes.bool,
+  openNav: PropTypes.func
+};
+
+export default MobileNavbar;
