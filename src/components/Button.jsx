e2723dcef92157c471a36049f5fd35b9d1ba271a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:38:55 +0300

    Created reusable Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+import icons from '../assets/images/icons.svg';
+
+const Button = ({ label, iconURL, ariaLabel, className, svgClass, onClick }) => {
+  return (
+    <button
+      className={
+        className
+          ? `${className}`
+          : 'flex h-[28px] w-[28px] items-center justify-center rounded-full border-none outline-none transition duration-200 ease-in-out active:shadow-sm'
+      }
+      onClick={onClick}
+    >
+      {label}
+
+      {iconURL && (
+        <svg width="18" height="18" aria-label={ariaLabel} className={svgClass}>
+          <use href={icons + iconURL} />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  label: PropTypes.string,
+  iconURL: PropTypes.string,
+  ariaLabel: PropTypes.string,
+  className: PropTypes.string,
+  svgClass: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+export default Button;
