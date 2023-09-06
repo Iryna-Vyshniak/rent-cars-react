74be0359bcf5f20766ceb255352a39c1f9dbaee5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 13:51:14 +0300

    Created 'Layout' component, which serves as the overall layout structure for the application. It includes a header, main content area, and supports code-splitting with a loading spinner

---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import { Suspense } from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+import Container from './Container';
+import Header from './Header/Header';
+import Spinner from './Spinner';
+
+const Layout = () => {
+  return (
+    <div className="relative flex min-h-screen flex-col items-center justify-center">
+      <Header />
+      <main className="mt-11 w-full grow">
+        <Container className="padding">
+          <Suspense fallback={<Spinner />}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
