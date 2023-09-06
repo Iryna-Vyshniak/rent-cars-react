c9e10950fb6765505a49df76fcc96c6c3e5a6923
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-06 14:04:05 +0300

    complete basename

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import UserRoutes from './UserRoutes';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/rent-cars-react">
       <UserRoutes />
     </BrowserRouter>
   );
