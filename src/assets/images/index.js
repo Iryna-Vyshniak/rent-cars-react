47afd12495f7119fa2af4689c8dc9dff2cec1f29
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-07 20:25:28 +0300

    add images

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import WhiteCar from './white-car.png';
+import DefaultCar from './car.jpg';
+import LogoCar from './logo.png';
+import Sgn from './sgn.png';
+import BgHero from './bg-hero-min.png';
+import BgFav from './fav-bg-min.png';
+
+export { DefaultCar, WhiteCar, Sgn, LogoCar, BgHero, BgFav };
