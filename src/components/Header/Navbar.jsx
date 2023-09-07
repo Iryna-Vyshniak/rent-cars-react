a3a6544b56c8f2fcf44d2d04e5a0b9b6594ffd07
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-07 20:41:25 +0300

    complete with hook

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,7 +2,7 @@ import icons from '../../assets/images/icons.svg';
 
 import Logo from '../Logo';
 
-import { useNavbar } from '../../shared/hooks';
+import { useToggle } from '../../shared/hooks';
 
 import Button from '../Button';
 
@@ -10,13 +10,13 @@ import MobileNavbar from './MobileNavbar';
 import DeskNavbar from './DeskNavbar';
 
 const Navbar = () => {
-  const { navbarOpen, toggleNavbar } = useNavbar();
+  const { isOpen, toggle } = useToggle(false);
 
   return (
     <nav>
-      <MobileNavbar icons={icons} navbar={navbarOpen} openNav={toggleNavbar} />
+      <MobileNavbar icons={icons} navbar={isOpen} openNav={toggle} />
       <DeskNavbar />
-      <div className="mobile-hamb" onClick={toggleNavbar}>
+      <div className="mobile-hamb" onClick={toggle}>
         <Logo text="RentCars" classText="logo-text" />
         <Button svgClass="mobile-navbar-menu" iconURL="#icon-menu" ariaLabel="menu" />
       </div>
