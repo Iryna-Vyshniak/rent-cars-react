d26aee512ef205b1d2b994e7c8b100051c5bfb36
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-07 20:48:04 +0300

    created components for enhance code modularity and reusability for displaying accessory and functionality lists: 'AccessoriesBlock' and 'AccessoriesItem'

---
+++ src/components/Accessories/AccessoriesBlock.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+import Separator from '../Separator';
+
+import AccessoriesItem from './AccessoriesItem';
+
+const AccessoriesBlock = ({ accessories, functionalities, title }) => {
+  return (
+    <div className="mb-[24px]">
+      <p className="modal-second-title">{title}:</p>
+      <AccessoriesItem items={accessories} separator={<Separator />} />
+      <AccessoriesItem items={functionalities} separator={<Separator />} />
+    </div>
+  );
+};
+
+AccessoriesBlock.propTypes = {
+  accessories: PropTypes.array.isRequired,
+  functionalities: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default AccessoriesBlock;
---
+++ src/components/Accessories/AccessoriesItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+const AccessoriesItem = ({ items, separator }) => {
+  return (
+    <ul className="mb-[4px] flex flex-wrap items-center justify-start">
+      {items.map((item, idx) => (
+        <li key={idx} className="text-description">
+          {item}
+          {idx < items.length - 1 && separator}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+AccessoriesItem.propTypes = {
+  items: PropTypes.array.isRequired,
+  separator: PropTypes.any
+};
+
+export default AccessoriesItem;
