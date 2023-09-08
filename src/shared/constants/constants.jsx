5c443d27e4dfafdf964e2ced9faaf5c52bc0cfb7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-08 14:14:02 +0300

    update active link

--- src/shared/constants/constants.jsx
+++ src/shared/constants/constants.jsx
@@ -1,4 +1,4 @@
 export const activeLink =
-  'cursor-pointer py-3 text-sm font-montserrat  font-semibold text-main-text focus:text-accent-color border-b-[3px] border-b-accent-color transition duration-200 ease-in-out';
+  'cursor-pointer py-3 text-sm font-montserrat  font-semibold  text-active-link focus:text-accent-color drop-shadow-[0_0.15px_0.15px_rgba(0,0,20,1)] border-b-[3px] border-b-accent-color transition duration-200 ease-in-out';
 
 export const COUNT_CARS = 8;
