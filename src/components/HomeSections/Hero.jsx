5f02da6cab4d828927f7d4d5174a38378d7a1014
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-10 17:16:55 +0300

    Improved code style for consistent formatting

--- src/components/HomeSections/Hero.jsx
+++ src/components/HomeSections/Hero.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import { HeroPoster } from '../../assets/images';
+import MainTitle from '../MainTitle';
 
 const Hero = () => {
   return (
@@ -11,12 +12,12 @@ const Hero = () => {
         alt="/"
       />
       <div className="absolute left-0 top-0 h-screen w-full bg-black/30" />
-      <div className="absolute left-0 top-[8rem] flex h-full w-full max-w-[1140px] flex-col items-start justify-start p-5 text-white md:left-[10%]">
+      <div className="absolute left-0 top-[8rem] flex h-full w-full max-w-[800px] flex-col items-start justify-start p-5 text-white md:left-[10%]">
         <p>All Inclusive</p>
-        <h1 className="responsive-font  mb-6 font-bold drop-shadow-2xl md:text-8xl">
+        <MainTitle className="mb-6 max-w-[950px] p-3 font-bold drop-shadow-2xl md:text-8xl">
           Your Key to <span className="gradient">Freedom</span> on Ukrainian Roads
-        </h1>
-        <p className="mb-4 max-w-[600px] py-2 text-[1.5rem] drop-shadow-2xl">
+        </MainTitle>
+        <p className="mb-4 max-w-[600px] py-2 text-[1.1rem] drop-shadow-2xl">
           Experience the freedom of exploring Ukraine at your own pace with our car rental app.
           Discover picturesque landscapes, vibrant cities, and hidden gems with ease. Your adventure
           begins here.
