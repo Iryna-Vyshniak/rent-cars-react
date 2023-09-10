93d1a52250d410a260a2ba1049c79556b65de91f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-10 05:18:12 +0300

    Completed with sidebar

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,13 @@
-const FavoritesPage = () => {
-  return <div>FavoritesPage</div>
-}
+import Sidebar from '../components/Sidebar/Sidebar';
 
-export default FavoritesPage
+const FavoritesPage = () => {
+  return (
+    <div className="fav-bg flex min-h-screen dark:bg-slate-800">
+      <div className="relative z-10 mx-auto my-0 flex items-center justify-center overflow-hidden bg-white/80">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
