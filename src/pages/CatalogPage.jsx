2a3fe5f573450d4a644b7682a202f738cd6fe786
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-10 05:16:56 +0300

    Implement 'CatalogPage' component

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,5 +1,36 @@
-const CatalogPage = () => {
-  return <div>CatalogPage</div>
-}
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default CatalogPage
+import CarsList from '../components/Cars/CarsList';
+import { selectCars, selectFilter } from '../redux/cars/carsSelectors';
+import { getAllCars } from '../redux/cars/carsThunk';
+
+import { filterCars } from '../shared/utils';
+import Filter from '../components/Filter/Filter';
+import { setFilter } from '../redux/cars/carsSlice';
+
+const CatalogPage = () => {
+  const dispatch = useDispatch();
+  const cars = useSelector(selectCars);
+
+  const filter = useSelector(selectFilter);
+
+  const filteredCars = filterCars(cars, filter);
+
+  useEffect(() => {
+    dispatch(getAllCars());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(setFilter(null));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Filter cars={filteredCars} />
+      {cars?.length > 0 && <CarsList adverts={filteredCars} />}
+    </>
+  );
+};
+
+export default CatalogPage;
