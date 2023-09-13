0577047c123c315b72574058b14f865414ddd900
Iryna-Vyshniak <Iryna-Vyshniak@users.noreply.github.com>
2023-09-13 14:18:10 +0000

    deploy: 312b77b9e70313a68f905dab8e07bd16a1ba4ba0

--- assets/FavoritesPage-d8a9b5c6.js
+++ assets/FavoritesPage-d8a9b5c6.js
@@ -1,4 +1,4 @@
-import{j as e,u as l,g as p,P as f,s as n,i as j,B as g,T as b}from"./index-812c85e7.js";import{T as v,b as o,a as m,C as N,u as w,c as C,F}from"./Filter-5dfee624.js";import{M as k}from"./MainTitle-7bb5d80d.js";const y="/rent-cars-react/assets/whitecar-3ba59620.png",T=()=>e.jsxs("section",{className:"no-scrollbar relative flex flex-col items-center justify-start overflow-hidden",children:[e.jsx(k,{className:`responsive-font relative z-[50] mt-0
+import{j as e,u as l,g as p,P as f,s as n,i as j,B as g,T as b}from"./index-bbd63b95.js";import{T as v,b as o,a as m,C as N,u as w,c as C,F}from"./Filter-65cc3139.js";import{M as k}from"./MainTitle-889a3788.js";const y="/rent-cars-react/assets/whitecar-3ba59620.png",T=()=>e.jsxs("section",{className:"no-scrollbar relative flex flex-col items-center justify-start overflow-hidden",children:[e.jsx(k,{className:`responsive-font relative z-[50] mt-0
         flex
         items-start
         justify-center
