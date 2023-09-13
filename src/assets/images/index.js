89307f807191c7cde8571cfb34dd94750e61ad0d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-13 17:14:27 +0300

    add img for banner

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,9 @@ import LogoCar from './logo.png';
 import Sgn from './sgn.png';
 import Car from './whitecar.png';
 import Nature from './nature.png';
+import Banner from './baner-main.png';
+import BMW from './bmw.png';
 
 import HeroPoster from './hero.jpg';
 
-export { DefaultCar, Sgn, LogoCar, Car, HeroPoster, Nature };
+export { DefaultCar, Sgn, LogoCar, Car, HeroPoster, Nature, Banner, BMW };
