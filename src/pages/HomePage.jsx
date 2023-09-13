312b77b9e70313a68f905dab8e07bd16a1ba4ba0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-13 17:17:13 +0300

    add banner component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Banner from '../components/HomeSections/Banner';
 import Cars from '../components/HomeSections/Cars';
 import Destinations from '../components/HomeSections/Destination';
 import Hero from '../components/HomeSections/Hero';
@@ -10,6 +11,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <Hero />
+      <Banner />
       <Destinations />
       <Offers />
       <RentService />
