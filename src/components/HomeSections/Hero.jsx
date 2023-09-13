3c488a2e548f5a1ad216d56170e3af18bf6e46a5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-13 18:24:32 +0300

    add styles

--- src/components/HomeSections/Hero.jsx
+++ src/components/HomeSections/Hero.jsx
@@ -4,8 +4,8 @@ import MainTitle from '../MainTitle';
 
 const Hero = () => {
   return (
-    <section className="hero mt-[-4rem] w-screen">
-      <div className="max-container absolute left-0 top-[8rem] flex h-full w-full flex-col items-start justify-start p-20 text-white">
+    <section className="hero mt-[-8rem] w-screen">
+      <div className="max-container absolute left-0 top-[8rem] mt-[2rem] flex h-full w-full flex-col items-start justify-start p-20 text-white">
         <p>All Inclusive</p>
         <MainTitle className="mb-6 max-w-[950px] p-3 font-bold drop-shadow-2xl md:text-8xl">
           Your Key to <span className="gradient">Freedom</span> on Ukrainian Roads
