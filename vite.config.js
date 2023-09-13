fdd4eed0b796ef3a3fc759f607e612aebbf39f16
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-13 17:29:14 +0300

    add resolve

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rent-cars-react'
+  base: '/rent-cars-react',
+  resolve: {
+    alias: { src: '/src' }
+  }
 });
